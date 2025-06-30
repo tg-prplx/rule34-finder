31cf8db93121292d65f330dac3ad6139a91a5327
prplx <whytomatic@gmail.com>
2025-06-30 19:46:39 +0100

    idk

--- PythonApplication1/config.py
+++ PythonApplication1/config.py
@@ -9,3 +9,4 @@ RULE_34_BACKEND_URL: str = os.getenv('RULE_34_BACKEND_URL', 'https://api.rule34.
 IMAGE_PLACEHOLDER_URL: str = os.getenv('PLACEHOLDER_IMAGE_URL', 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png')
 TAGS_PATH: str = os.getenv('TAGS_PATH', 'tags.json')
 GROUP_COUNT: int = 14
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
